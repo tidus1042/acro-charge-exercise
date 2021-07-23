ea15d767f7236eeb91fdfb500cc93d2d79bdea73
Asaf Sabag <sabag.asaf.1@gmail.com>
2021-07-23 12:12:12 +0300

    Added data for mocking

---
+++ db-broker/database/initializeDB.ts
@@ -0,0 +1,31 @@
+import { Customer, CustomerModel } from '../database/customerSchema'
+import { Transaction, TransactionModel } from "../database/transactionSchema";
+
+export const transactionsData = require('./data.json');
+
+export const initializeData = () => {
+    transactionsData.forEach((transactionData) => {
+        const customer: object = {
+            _id: transactionData.customer_id,
+            firstName: transactionData.first_name,
+            lastName: transactionData.last_name,
+            email: transactionData.email,
+            gender: transactionData.gender,
+            country: transactionData.country,
+            city: transactionData.city,
+            street: transactionData.street,
+            phone: transactionData.phone,
+        };
+        CustomerModel.create(customer);
+
+        let transaction: object = {
+            totalPrice: transactionData.total_price,
+            currency: transactionData.currency,
+            creditCardType: transactionData.cerdit_card_type,
+            creditCardNumber: transactionData.cerdit_card_number,
+            customerId: transactionData.customer_id,
+        };
+
+        TransactionModel.create(transaction);
+    });
+};
